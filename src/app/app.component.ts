6a060223fabd069f1a4c708dfe3df90a2f24d402
raam877 <raam_202100413@smit.smu.edu.in>
2023-09-26 22:39:40 -0700

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,3 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'Hello';
-//   inputValue:string = "initial value";
-//   todo =['Execise','Breakfast','class','Lunch'];
-//   addJob(job : string){
-//     this.todo.push(job);
-//   }
-//   deleteJob(job : string){
-//     for (let i = 0; i < this.todo.length; i++)
-//     {
-//       if(this.todo[i] === job){
-//         delete this.todo[i];
-//       }
-//     }
-//   }
-// }
-
 import { Component } from '@angular/core';
 
 @Component({
@@ -30,13 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "todoApp";
-  tasks: string[] = [];
+  tasks: string[] = ['Sleep'];
 
   addTask(newTask: string) {
-    if (newTask) {
       this.tasks.push(newTask);
-    }
   }
 
   deleteTask(index: number) {
