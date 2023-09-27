6743d2cd067e47c6c120c44603dc59fe6d778ce1
raam877 <raam_202100413@smit.smu.edu.in>
2023-09-26 22:15:22 -0700

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,27 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'Hello';
+//   inputValue:string = "initial value";
+//   todo =['Execise','Breakfast','class','Lunch'];
+//   addJob(job : string){
+//     this.todo.push(job);
+//   }
+//   deleteJob(job : string){
+//     for (let i = 0; i < this.todo.length; i++)
+//     {
+//       if(this.todo[i] === job){
+//         delete this.todo[i];
+//       }
+//     }
+//   }
+// }
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +30,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist';
+  title = "todoApp";
+  tasks: string[] = [];
+
+  addTask(newTask: string) {
+    if (newTask) {
+      this.tasks.push(newTask);
+    }
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
@@ -10,9 +11,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
